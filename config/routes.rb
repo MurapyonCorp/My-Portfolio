09665f1b074f0ee45b2cf3473d732bcad3fa1646
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-07-05 09:56:57 +0000

    devise complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get "home/about" => "homes#about"
+  root to: 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
