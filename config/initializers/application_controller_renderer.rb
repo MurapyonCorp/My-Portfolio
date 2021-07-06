8e33d393b9e0978b4efa1ed439ddd7ae5485044b
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-07-06 11:52:25 +0000

    user機能終了

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '3ddf86d5d14513408422c4e52ed8918fba76daa8a48ea1b2194b781ed898a43e8d76a654bd0fadafee954f9f3eefbee06d0a1c1ab719caa4cdec8f1e4253c904'
