ec9f112a08a9d42298334e3eb835c64f0bad6133
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-07-06 03:21:25 +0000

    レイアウト調整

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!,except: [:top, :about]
+
+  # ログイン後にヘッダーにユーザー名を表示させる
+  before_action :get_current_user
+  def get_current_user
+    @user = current_user
+  end
+
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
+
   protected
-  
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
   end
