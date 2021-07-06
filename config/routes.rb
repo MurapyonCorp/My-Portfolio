4349bd626c0ce8ffe8a5ba4a05b4eaac11204ece
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-07-06 03:13:11 +0000

    user ルート追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   get "home/about" => "homes#about"
   root to: 'homes#top'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:index, :show, :edit, :update]
 end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
