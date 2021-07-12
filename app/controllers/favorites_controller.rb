56e8013ff480fbdb81d0ac1960b5c6f329c5f666
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-07-12 13:39:28 +0900

    非同期完了

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,15 @@
 class FavoritesController < ApplicationController
+  before_action :authenticate_user!
+
   def create
-    event = Event.find(params[:event_id])
-    favorite = current_user.favorites.new(event_id: event.id)
+    @event = Event.find(params[:event_id])
+    favorite = current_user.favorites.new(event_id: @event.id)
     favorite.save
   end
 
   def destroy
-    event = Event.find(params[:event_id])
-    favorite = current_user.favorites.find_by(event_id: event.id)
+    @event = Event.find(params[:event_id])
+    favorite = current_user.favorites.find_by(event_id: @event.id)
     favorite.destroy
   end
 end
