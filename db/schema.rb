22ada5a50fb6f8915c7ed66881af76514ea6de59
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-07-14 17:44:32 +0900

    delete no file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_12_115042) do
+ActiveRecord::Schema.define(version: 2021_07_14_055748) do
 
   create_table "event_comments", force: :cascade do |t|
     t.text "comment"
