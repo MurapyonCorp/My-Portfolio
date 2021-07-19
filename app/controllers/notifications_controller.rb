6b247c58b19c43abd5500f36b693c7060968aeda
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-07-20 00:09:22 +0900

    includes bullet warning 解除

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,7 +1,7 @@
 class NotificationsController < ApplicationController
   def index
     #current_userの投稿に紐づいた通知一覧
-      @notifications = current_user.passive_notifications
+      @notifications = current_user.passive_notifications.page(params[:page])
     #@notificationの中でまだ確認していない(indexに一度も遷移していない)通知のみ
       @notifications.where(checked: false).each do |notification|
         notification.update_attributes(checked: true)
