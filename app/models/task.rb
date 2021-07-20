28f5903276c38eeddbb5a32e50fbb400b0f36479
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-07-20 14:31:03 +0900

    task validation add

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,11 @@ class Task < ApplicationRecord
   belongs_to :user
   enum pratical: { 実施済: true, 未実施: false }
   has_many :likes, dependent: :destroy
+  
+  validates :title, presence: true, length: { maximum: 100 }
+  validates :body, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
 
   def liked_by?(user)
     likes.where(user_id: user.id).exists?
