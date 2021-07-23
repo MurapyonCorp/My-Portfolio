49bfa7a3222c6d9d6a0f0d94f4df607ca91d7bad
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-07-23 10:00:58 +0900

    taskモデルテスト作成

---
+++ spec/models/task_spec.rb
@@ -0,0 +1,67 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+Rspec.describe 'Taskモデルのテスト', type: :model do
+  describe 'バリデーションのテスト' do
+    subject { task.valid? }
+
+    let(:user) { create(:user) }
+    let!(:task) { build(:task, user_id: user.id) }
+
+    context 'titleカラム' do
+      it '空欄でないこと' do
+        task.title = ''
+        is_expected.to eq false
+      end
+      it '100文字以下であること: 100文字はOK' do
+        task.title = Faker::Lorem.characters(number: 100)
+        is_expected.to eq true
+      end
+      it '100文字以下であること: 101文字はNG' do
+        task.title = Faker::Lorem.characters(number: 101)
+        is_expected.to eq false
+      end
+    end
+    context 'bodyカラム' do
+      it '空欄でないこと' do
+        task.body = ''
+        is_expected.to eq false
+      end
+    end
+    context 'start_dateカラム' do
+      it '空欄でないこと' do
+        task.start_date = ''
+        is_expected.to eq false
+      end
+    end
+    context 'end_dateカラム' do
+      it '空欄でないこと' do
+        task.end_date = ''
+        is_expected.to eq false
+      end
+    end
+  end
+  describe 'アソシエーションのテスト' do
+    context 'Userモデルとの関係' do
+      it '1:Nの関係になっている' do
+        expect(Task.reflect_on_association(:user).macro).to eq :belongs_to
+      end
+    end
+    context 'TaskCommentモデルとの関係' do
+      it '1:Nの関係になっている' do
+        expect(Task.reflect_on_association(:task_comment).macro).to eq :has_many
+      end
+    end
+    context 'Notificationモデルとの関係' do
+      it '1:Nの関係になっている' do
+        expect(Task.reflect_on_association(:notification).macro).to eq :has_many
+      end
+    end
+    context 'Likeモデルとの関係' do
+      it '1:Nの関係になっている' do
+        expect(Task.reflect_on_association(:like).macro).to eq :has_many
+      end
+    end
+  end
+end
