8d83c9ea293dce7403a0c0737b996868a865b9c4
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-07-23 13:23:03 +0900

    relationshipモデルテスト作成完了

---
+++ spec/models/relationship_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+Rspec.describe 'Relationshipモデルのテスト', type: :model do
+  describe 'アソシエーションのテスト' do
+    context 'Userモデルとの関係' do
+      it '1:Nの関係になっている' do
+        expect(Relationship.reflect_on_association(:follower).macro).to eq :belongs_to
+      end
+    end
+    context 'Userモデルとの関係' do
+      it '1:Nの関係になっている' do
+        expect(Relationship.reflect_on_association(:followed).macro).to eq :belongs_to
+      end
+    end
+  end
+end
