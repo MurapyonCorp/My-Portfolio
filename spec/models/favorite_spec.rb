00c9745c2b13cbec01cf04f52ea29d9b20845450
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-07-23 13:21:55 +0900

    favoriteモデルテスト作成完了

---
+++ spec/models/favorite_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+Rspec.describe 'Favoriteモデルのテスト', type: :model do
+  describe 'アソシエーションのテスト' do
+    context 'Userモデルとの関係' do
+      it '1:Nの関係になっている' do
+        expect(Favorite.reflect_on_association(:user).macro).to eq :belongs_to
+      end
+    end
+    context 'Eventモデルとの関係' do
+      it '1:Nの関係になっている' do
+        expect(Favorite.reflect_on_association(:event).macro).to eq :belongs_to
+      end
+    end
+  end
+end
