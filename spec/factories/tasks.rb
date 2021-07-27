96f1c8fcc236b998f99cc43a65205ceb36868f27
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-07-28 00:21:38 +0900

    pratical編集

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -2,7 +2,6 @@ FactoryBot.define do
   factory :task do
     title { Faker::Lorem.characters(number: 10) }
     body { Faker::Lorem.characters(number: 50) }
-    pratical { Faker::Boolean.boolean(true_ratio: 0.2) }
     start_date { Faker::Time.between(from: DateTime.now - 1, to: DateTime.now) }
     end_date { Faker::Time.between(from: DateTime.now - 1, to: DateTime.now) }
     user
