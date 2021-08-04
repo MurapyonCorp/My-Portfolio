817a68dd72b84019846c2a0940dbf684901882a2
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-08-04 17:28:05 +0900

    task faker 編集

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -2,8 +2,8 @@ FactoryBot.define do
   factory :task do
     title { Faker::Lorem.characters(number: 10) }
     body { Faker::Lorem.characters(number: 50) }
-    start_date { Faker::Time.between(from: DateTime.now - 1, to: DateTime.now) }
-    end_date { Faker::Time.between(from: DateTime.now - 1, to: DateTime.now) }
+    start_date { Faker::Time.between(from: DateTime.now + 1, to: DateTime.now + 1) }
+    end_date { Faker::Time.between(from: DateTime.now + 2, to: DateTime.now + 2) }
     user
   end
 end
