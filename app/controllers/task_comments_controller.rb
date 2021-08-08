ec83bd0cafd95c51187d2fb9ddad03740472b356
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-08-08 17:20:38 +0900

    task_comment 通知バリデーション完了

--- app/controllers/task_comments_controller.rb
+++ app/controllers/task_comments_controller.rb
@@ -1,13 +1,19 @@
 class TaskCommentsController < ApplicationController
   def create
-    task = Task.find(params[:task_id])
-    comment = current_user.task_comments.new(task_comment_params)
-    comment.task_id = task.id
-    comment.save
-    @task = comment.task
-    @task_comments = TaskComment.includes(:task, :user).where(task_id: params[:task_id])
-    # 通知の作成
-    @task.create_notification_task_comment!(current_user, comment.id)
+    @task = Task.find(params[:task_id])
+    @comment = current_user.task_comments.new(task_comment_params)
+    @comment.task_id = @task.id
+    if @comment.save
+      @task = @comment.task
+      @task_comments = TaskComment.includes(:task, :user).where(task_id: params[:task_id])
+      # 通知の作成
+      @task.create_notification_task_comment!(current_user, @comment.id)
+    else
+      @user = @task.user
+      @task_comments = TaskComment.includes(:user).where(task_id: @task.id)
+      @task_comment = TaskComment.new
+      render template: "tasks/show"
+    end
   end
 
   def destroy
