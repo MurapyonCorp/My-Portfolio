4f269c224d15d71e21a9250adc0cbaecbc273dc0
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-08-21 17:33:02 +0900

    user-showにroom作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,24 @@ class UsersController < ApplicationController
       notification = Notification.find_by!(visited_id: current_user.id, visiter_id: @user.id, action: "follow") #ここでNotificationモデルから左記の3つの情報を元に誰からの通知なのかを特定する。
       notification.update!(checked: true)
     end
+    @currentUserEntry = Entry.where(user_id: current_user.id)
+    @userEntry = Entry.where(user_id: @user.id)
+    if @user.id == current_user.id
+    else
+      @currentUserEntry.each do |current_user|
+        @userEntry.each do |user|
+          if current_user.room_id == user.room_id then
+            @isRoom = true
+            @roomId = current_user.room_id
+          end
+        end
+      end
+      if @isRoom
+      else
+        @room = Room.new
+        @entry = Entry.new
+      end
+    end
   end
 
   def edit
