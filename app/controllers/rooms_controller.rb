8d21d6ffa390f735b899781357bccca83fb3e478
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-08-22 21:41:05 +0900

    遷移先表記変更

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -5,7 +5,7 @@ class RoomsController < ApplicationController
     @room = Room.create
     @entry1 = Entry.create(:room_id => @room.id, :user_id => current_user.id)
     @entry2 = Entry.create(params.require(:entry).permit(:user_id, :room_id).merge(:room_id => @room.id))
-    redirect_to "/rooms/#{@room.id}"
+    redirect_to room_path(@room.id)
   end
 
   def show
