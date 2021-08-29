e95c620c05b4020988516c29d9b389213854fb36
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-08-30 08:58:41 +0900

    controller修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,9 +2,10 @@ class MessagesController < ApplicationController
   before_action :authenticate_user!, :only => [:create]
 
   def create
-    if Entry.where(:user_id => current_user.id, :room_id => params[:message][:room_id]).present?
+    if Entry.where(user_id: current_user.id, room_id: params[:message][:room_id]).present?
       @message = Message.create(message_params)
-      @messages = Message.includes(:user)
+      @room = Room.find(params[:message][:room_id])
+      @messages = Message.includes(:user).where(room_id: @room.id)
     else
       redirect_back(fallback_location: users_path)
     end
