006b29abfe6a7f745898bd593ad81a6a5c44347f
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-08-29 16:01:30 +0900

    redirect表記変更

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,7 +4,7 @@ class MessagesController < ApplicationController
   def create
     if Entry.where(:user_id => current_user.id, :room_id => params[:message][:room_id]).present?
       @message = Message.create(params.require(:message).permit(:user_id, :content, :room_id).merge(:user_id => current_user.id))
-      redirect_to "/rooms/#{@message.room_id}"
+      redirect_to room_path(@message.room_id)
     else
       redirect_back(fallback_location: users_path)
     end
