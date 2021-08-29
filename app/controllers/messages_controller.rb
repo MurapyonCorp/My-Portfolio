16cca3901962b931122694264e2db36e8e9bba65
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-08-30 07:49:38 +0900

    redirectを削除

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,7 +4,7 @@ class MessagesController < ApplicationController
   def create
     if Entry.where(:user_id => current_user.id, :room_id => params[:message][:room_id]).present?
       @message = Message.create(message_params)
-      redirect_to room_path(@message.room_id)
+      @messages = Message.includes(:user)
     else
       redirect_back(fallback_location: users_path)
     end
@@ -13,7 +13,7 @@ class MessagesController < ApplicationController
   def destroy
     message = Message.find(params[:id])
     message.destroy
-    redirect_to request.referer
+    @messages = Message.includes(:user)
   end
 
   private
