38dfdf1bce6c3e2f02e345ee2c99bea82e8c24cc
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-08-29 17:36:10 +0900

    strong parameter追加

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,7 +3,7 @@ class MessagesController < ApplicationController
 
   def create
     if Entry.where(:user_id => current_user.id, :room_id => params[:message][:room_id]).present?
-      @message = Message.create(params.require(:message).permit(:user_id, :content, :room_id).merge(:user_id => current_user.id))
+      @message = Message.create(message_params)
       redirect_to room_path(@message.room_id)
     else
       redirect_back(fallback_location: users_path)
@@ -14,5 +14,11 @@ class MessagesController < ApplicationController
     message = Message.find(params[:id])
     message.destroy
     redirect_to request.referer
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:user_id, :content, :room_id).merge(user_id: current_user.id)
   end
 end
