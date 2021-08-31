4664c603258789ec0933fa19950acdb2e4c38554
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-08-31 14:57:54 +0900

    通知バリデーション追加

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,6 +6,16 @@ class MessagesController < ApplicationController
       @message = Message.create(message_params)
       @room = Room.find(params[:message][:room_id])
       @messages = Message.includes(:user).where(room_id: @room.id)
+      if @message.save
+        # 通知を作成
+        @message_room = @message.room
+        @message_room.create_notification_message!(current_user, @message.id)
+      else
+        @messages = @room.messages.includes(:user)
+        @message = Message.new
+        @entries = @room.entries.includes(:user).where.not(user_id: current_user.id)
+        render template: "rooms/show"
+      end
     else
       redirect_back(fallback_location: users_path)
     end
