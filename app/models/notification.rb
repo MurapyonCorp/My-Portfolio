c4f0de907b5cefc327cf928905e18539fb6a8049
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-08-31 15:01:59 +0900

    room、messageとのアソシエーションを追記

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -6,6 +6,8 @@ class Notification < ApplicationRecord
   belongs_to :task, optional: true
   belongs_to :event_comment, optional: true
   belongs_to :task_comment, optional: true
+  belongs_to :room, optional: true
+  belongs_to :message, optional: true
   belongs_to :visiter, class_name: 'User', foreign_key: 'visiter_id', optional: true
   belongs_to :visited, class_name: 'User', foreign_key: 'visited_id', optional: true
 end
