1ea4d886eefeb38a0e49e147b6da3574ba84638a
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-09-01 17:04:12 +0900

    通知の削除機能追加

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -16,6 +16,11 @@ class LikesController < ApplicationController
   def destroy
     @task = Task.find(params[:task_id])
     like = current_user.likes.find_by(task_id: @task.id)
+    @task.destroy_notification_by(current_user)
+    respond_to do |format|
+      format.html { redirect_to request.referer }
+      format.js
+    end
     like.destroy
   end
 end
