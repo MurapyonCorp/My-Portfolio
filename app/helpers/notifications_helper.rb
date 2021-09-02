af16e199b0b4c435dfaf472071dae0dc8f9b6d61
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-09-02 11:07:46 +0900

    あなたの投稿を投稿のタイトルに変更

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -12,15 +12,15 @@ module NotificationsHelper
     when "follow"
       tag.a(notification.visiter.name, href: user_path(@visiter, checked: true), style: "font-weight: bold;") + "があなたをフォローしました"
     when "favorite"
-      tag.a(notification.visiter.name, href: user_path(@visiter), style: "font-weight: bold;") + "が" + tag.a('あなたの投稿', href: event_path(notification.event_id, checked: true), style: "font-weight: bold;") + "にいいねしました"
+      tag.a(notification.visiter.name, href: user_path(@visiter), style: "font-weight: bold;") + "が" + tag.a(notification.event.title, href: event_path(notification.event_id, checked: true), style: "font-weight: bold;") + "にいいねしました"
     when "like"
-      tag.a(notification.visiter.name, href: user_path(@visiter), style: "font-weight: bold;") + "が" + tag.a('あなたの投稿', href: task_path(notification.task_id, checked: true), style: "font-weight: bold;") + "にいいねしました"
+      tag.a(notification.visiter.name, href: user_path(@visiter), style: "font-weight: bold;") + "が" + tag.a(notification.task.title, href: task_path(notification.task_id, checked: true), style: "font-weight: bold;") + "にいいねしました"
     when "event_comment" then
       @event_comment = EventComment.find_by(id: @visiter_comment)
-      tag.a(@visiter.name, href: user_path(@visiter), style: "font-weight: bold;") + "が" + tag.a('あなたの投稿', href: event_path(notification.event_id, checked: true), style: "font-weight: bold;") + "にコメントしました"
+      tag.a(@visiter.name, href: user_path(@visiter), style: "font-weight: bold;") + "が" + tag.a(notification.event.title, href: event_path(notification.event_id, checked: true), style: "font-weight: bold;") + "にコメントしました"
     when "task_comment" then
       @task_comment = TaskComment.find_by(id: @visiter_comment)
-      tag.a(@visiter.name, href: user_path(@visiter), style: "font-weight: bold;") + "が" + tag.a('あなたの投稿', href: task_path(notification.task_id, checked: true), style: "font-weight: bold;") + "にコメントしました"
+      tag.a(@visiter.name, href: user_path(@visiter), style: "font-weight: bold;") + "が" + tag.a(notification.task.title, href: task_path(notification.task_id, checked: true), style: "font-weight: bold;") + "にコメントしました"
     when "DM" then
       @message = Message.find_by(id: notification.message_id)
       tag.a(@visiter.name, href: user_path(@visiter), style: "font-weight: bold;") + "があなたに" + tag.a('メッセージ', href: room_path(notification.room_id, checked: true), style: "font-weight: bold;") + "を送りました"
