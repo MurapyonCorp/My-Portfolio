c490e85a4ad4e2c45a0eefb8969dd1e49d7580f8
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-09-02 11:06:45 +0900

    重複レコードをまとめる

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,7 +1,7 @@
 class NotificationsController < ApplicationController
   def index
     # current_userの投稿に紐づいた通知一覧
-    @notifications = current_user.passive_notifications.page(params[:page])
+    @notifications = current_user.passive_notifications.select(:visiter_id, :event_id, :task_id, :room_id).distinct.page(params[:page])
     # @notificationの中でまだ確認していない(indexに一度も遷移していない)通知のみ
     @notifications.where(checked: false).each do |notification|
       notification.update_attributes(checked: true)
